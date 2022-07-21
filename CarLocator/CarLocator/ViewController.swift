d16cfc6b2308b2dbe0ed585fce203982940bc972
Marcos García Rouco <garciaroucomarcos@gmail.com>
2022-07-21 17:12:40 +0200

    [car_list] Initial commit

---
+++ CarLocator/CarLocator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarLocator
+//
+//  Created by Marcos García Rouco on 21/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
